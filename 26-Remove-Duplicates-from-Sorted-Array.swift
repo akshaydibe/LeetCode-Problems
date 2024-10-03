e468c018b72a154b740ec629dc1e276e028b7403
akshaydibe <58762059+akshaydibe@users.noreply.github.com>
2024-10-03 23:18:39 +0530

    [26] [Time Beats: 45.61%] [Memory Beats: 77.22%] - LeetPush

--- 26-Remove-Duplicates-from-Sorted-Array.swift
+++ 26-Remove-Duplicates-from-Sorted-Array.swift
@@ -3,24 +3,32 @@ class Solution {
         guard !nums.isEmpty else { return 0 }
 
         let n = nums.count
-        var left = 0
-        var right = 0
-        var k = 1
+        var idx = 0
 
-        while(right < n) {
-            if nums[right] == nums[left] {
-                right += 1
-            } else {
-                let temp = nums[right]
-                nums[right] = nums[left + 1]
-                nums[left + 1] = temp
-                left += 1
-                right += 1
-                k += 1
-            }
+        for num in nums where num != nums[idx] {
+            idx += 1
+            nums[idx] = num
         }
 
-        return k
+        return idx + 1
+        // var left = 0
+        // var right = 0
+        // var k = 1
+
+        // while(right < n) {
+        //     if nums[right] == nums[left] {
+        //         right += 1
+        //     } else {
+        //         let temp = nums[right]
+        //         nums[right] = nums[left + 1]
+        //         nums[left + 1] = temp
+        //         left += 1
+        //         right += 1
+        //         k += 1
+        //     }
+        // }
+
+        // return k
         
     }
 }
