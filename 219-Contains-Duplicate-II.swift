f7422ae437f82beffdd1793d4a77d03a59ae6e5d
akshaydibe <58762059+akshaydibe@users.noreply.github.com>
2024-10-04 16:10:47 +0530

    [219] [Time Beats: 60.60%] [Memory Beats: 44.48%] - LeetPush

---
+++ 219-Contains-Duplicate-II.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+        guard !nums.isEmpty else { return false }
+        var dict = [Int: Int]()
+
+        for i in 0..<nums.count {
+            if let idx = dict[nums[i]] {
+                if i - idx <= k {
+                    return true
+                }
+            }
+
+            dict[nums[i]] = i
+        }
+
+        return false
+    }
+}
